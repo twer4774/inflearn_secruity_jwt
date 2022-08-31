62a906925e8f14d16c23e5979b926cc09958ee6c
wonik <twer4774@gmail.com>
2022-08-31 20:24:23 +0900

    Init Project

---
+++ src/main/java/study/walter/inflearn_secruity_jwt/InflearnSecruityJwtApplication.java
@@ -0,0 +1,13 @@
+package study.walter.inflearn_secruity_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSecruityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnSecruityJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/walter/inflearn_secruity_jwt/InflearnSecruityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package study.walter.inflearn_secruity_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSecruityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
