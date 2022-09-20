3c73133b40d986d96341f96a288f209dde4da568
wonik <twer4774@gmail.com>
2022-09-20 13:27:05 +0900

    setting : change to multi modules

---
+++ jwt-exam/src/main/java/study/walter/inflearn_security_jwt/JwtApplication.java
@@ -0,0 +1,13 @@
+package study.walter.inflearn_security_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(JwtApplication.class, args);
+    }
+
+}
